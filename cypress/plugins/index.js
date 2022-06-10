e18d1cc5e2b9b98b234cf98c0a0e7171eaa6cbd8
Artem Minsadyrov <artem.mindsadyrov@actumdigital.com>
2022-06-10 15:29:30 +0200

    login

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,6 +2,10 @@
 
 const cucumber = require('cypress-cucumber-preprocessor').default
 
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
 }
